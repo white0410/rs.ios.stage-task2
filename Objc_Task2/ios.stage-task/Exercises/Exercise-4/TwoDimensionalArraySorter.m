d1b14ffe55f1cbc5047d7c5230fb48e49216256c
Andrei <andrei@Andreis-MacBook-Pro.local>
2021-05-17 22:59:33 +0300

    exercise4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,36 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if (array.count == 0 || !array || ![array[0] isKindOfClass:NSArray.class]) {
+            return @[];
+        }
+        NSMutableArray *res = [NSMutableArray array];
+        NSMutableArray *nums = [NSMutableArray array];
+        NSMutableArray *strings = [NSMutableArray array];
+        for (int i = 0; i < array.count; i++) {
+            if ([array[i][0] isKindOfClass:NSNumber.class]) {
+                [nums addObjectsFromArray:array[i]];
+            } else {
+                [strings addObjectsFromArray:array[i]];
+            }
+        }
+        if (nums.count > 0) {
+            nums = [[nums sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+            if (strings.count == 0) {
+                return [nums copy];
+            }
+        }
+        if (strings.count > 0) {
+            strings = [[strings sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+            if (nums.count == 0) {
+                return [strings copy];
+            }
+        }
+        [res addObject:[nums copy]];
+        [res addObject:[[strings reverseObjectEnumerator] allObjects]];
+    
+    return [res copy];
+    
 }
 
 @end
