9d0bef99ec296c000927976008ffecdc61877079
Andrei <andrei@Andreis-MacBook-Pro.local>
2021-05-17 22:45:36 +0300

    exercise1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,15 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    if (n == 0){
+        
+    }
+    UInt8 res = 0;
+    int c = 8;
+    while (c --) {
+        res = res * 2 + (n % 2);
+        n = n / 2;
+    }
+    
+    return res;
 }
