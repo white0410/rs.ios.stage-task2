73c85bac384fe4b36ed2533131ed525f4bc52403
Andrei <andrei@Andreis-MacBook-Pro.local>
2021-05-17 22:49:33 +0300

    exercise2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,6 +3,14 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
+    NSInteger result = 0;
+        for (NSInteger i = 0; i < array.count; i++) {
+            for (NSInteger j = i + 1; j < array.count; j++) {
+                if (ABS(array[i].intValue - array[j].intValue) == number.intValue ) {
+                    result++;
+                }
+            }
+        }
     return 0;
 }
 
